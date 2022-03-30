a263cb39981195f3ba2b904710c0c1e3b85cf08d
WeberFlm <97299828+WeberFlm@users.noreply.github.com>
2022-03-30 15:19:08 +0200

    new files

---
+++ Micromerce Dev-Test/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    'customer/index': './customer/index.ts',
+    'studio/index': './studio/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
+  },
+};
